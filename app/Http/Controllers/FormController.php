9cefa740c893e771ab9f1c922390c99dec4a4f9b
degawl <devongawley@gmail.com>
2020-01-07 13:44:25 +0200

    Add form controller

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+use App\User;
+use App\Sector;
+
+use Illuminate\Support\Facades\DB;
+
+class FormController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Form Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling handling all
+    | form related functions such as populating select box values
+    | and sending form data to DB.
+    |
+    */
+
+
+
+}
