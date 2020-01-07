a481eef0d26620cb7373349fb34b55a9ea5160fe
degawl <devongawley@gmail.com>
2020-01-07 22:38:58 +0200

    Add more feature tests

--- tests/Feature/FormTest.php
+++ tests/Feature/FormTest.php
@@ -74,7 +74,7 @@ class FormTest extends TestCase
     }
 
     /**
-     * A test to make sure loggin in users can /logout.
+     * A test to make sure logged in users can /logout.
      *
      * @return void
      */
@@ -83,6 +83,64 @@ class FormTest extends TestCase
         $user = factory(User::class)->create();
         $response = $this->actingAs($user)->post('/logout');
         $response->assertStatus(302);
+    }
+
+    /**
+     * A test to make sure users can update their names.
+     *
+     * @return void
+     */
+    public function testLoggedInUserCanUpdateName()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->post('/submitform', [
+            'name'               => 'Test Name',
+            'sectors'            => [1, 2, 3],
+            'agreement_to_terms' => 1
+        ]);
+
+        $name = DB::table('users')->pluck('name');
+        $this->assertTrue($name == '["Test Name"]');
+
+        $response = $this->actingAs($user)->post('/submitform', [
+            'name'               => 'Not Test Name',
+            'sectors'            => [2, 3, 4],
+            'agreement_to_terms' => 1
+        ]);
+        $changedName = DB::table('users')->pluck('name');
+        $this->assertTrue($changedName == '["Not Test Name"]');
+
+
+        $this->assertTrue($name != $changedName);
+    }
+
+    /**
+     * A test to make sure users can update their sectors.
+     *
+     * @return void
+     */
+    public function testLoggedInUserCanUpdateSectors()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->post('/submitform', [
+            'name'               => 'Test Name',
+            'sectors'            => [1, 2, 3],
+            'agreement_to_terms' => 1
+        ]);
+
+        $sectors = DB::table('user_sectors')->where('username', $user->username)->pluck('sector_id');
+        $this->assertTrue($sectors == '["1","2","3"]');
+
+        $response = $this->actingAs($user)->post('/submitform', [
+            'name'               => 'Test Name',
+            'sectors'            => [4, 5, 6],
+            'agreement_to_terms' => 1
+        ]);
+        $changedSectors = DB::table('user_sectors')->where('username', $user->username)->pluck('sector_id');
+        $this->assertTrue($changedSectors == '["4","5","6"]');
+
+        $this->assertTrue($sectors != $changedSectors);
+
     }
     
 }
