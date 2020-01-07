2177eff6a200ae71aa19515711a30faf932bc120
degawl <devongawley@gmail.com>
2020-01-07 14:02:30 +0200

    Add select and checkbox to form

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <div class="card">
+            <div class="card shadow">
                 <div class="card-header">Sectors Form</div>
 
                 <div class="card-body">
@@ -26,13 +26,29 @@
                         </div>
                     @endforeach
 
-                    <form action="" method="POST">
+                    <form action="submitform" method="POST">
                     @csrf
                         <div class="form-group">
                             <label for="name">Name</label>
-                            <input type="text" class="form-control" placeholder="Benjamin Button" name="name">
+                            <input type="text" class="form-control" placeholder="Benjamin Button" name="name" value="{{ $data['name'] }}">
                         </div>
-                        
+                        <div class="form-group">
+                            <label for="sectors[]">Sectors</label>
+                            <select name="sectors[]" class="form-control js-example-basic-multiple" multiple="multiple">
+                                @foreach ($data['sectors'] as $sector)
+                                    <option 
+                                        @if (in_array($sector->sector_id, $data['selectedSectors']))
+                                            selected="selected"
+                                        @endif
+                                        value="{{ $sector->sector_id }}">{{ $sector->sector_name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="form-group custom-control custom-checkbox mb-4">
+                            <input name="agreementToTerms" type="checkbox" class="custom-control-input" id="customCheck1">
+                            <label class="custom-control-label" for="customCheck1">Do you agree to the terms?</label>
+                        </div>
                         
                         <button type="submit" class="btn btn-primary">Save</button>
                     </form>
@@ -41,4 +57,5 @@
         </div>
     </div>
 </div>
+<script src="{{ asset('js/form.js') }}"></script>
 @endsection
