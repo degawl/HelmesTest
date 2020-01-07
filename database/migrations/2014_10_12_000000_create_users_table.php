b5b23ef3923ab5ab74b9168003fdefa07d47a165
degawl <devongawley@gmail.com>
2020-01-07 13:05:16 +0200

    Create users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('name')->nullable();
+            $table->tinyInteger('agreementToTerms')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
